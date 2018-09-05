6deb1c11643c3342baaff74c9705b02c5069265d
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-09-05 18:43:51 +0530

    Update StringFunctions.java

--- Week 02/StringFunctions.java
+++ Week 02/StringFunctions.java
@@ -5,12 +5,12 @@ String s2="world";
 String s3="   hello    ";
 String s4="HELLO";
 System.out.println("string length is: "+s1.length());  
-System.out.println("string length is: "+s2.length());
-System.out.println(s3.trim()+" how are you");
-String replaceString=s1.replace('h','t'); 
-System.out.println(replaceString);
+System.out.println("string length is: "+s2.length()); //demonstrates length()
+System.out.println(s3.trim()+" how are you");//demonstrates trim()
+String replaceString=s1.replace('h','t');
+System.out.println(replaceString);//demonstrates replace()
 System.out.println(s1.contains("hello"));
-System.out.println(s1.contains("fine"));
+System.out.println(s1.contains("fine"));//demonstrates contains()
 System.out.println(s1.equalsIgnoreCase(s2));
-System.out.println(s1.equalsIgnoreCase(s4));
+System.out.println(s1.equalsIgnoreCase(s4));//demonstrates equals()
 }}
